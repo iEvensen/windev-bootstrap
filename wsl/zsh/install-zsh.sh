36d260eef17f2eadf31ba1ff00b5b5de2167f7c2
iEvensen <idar.evensen@hnikt.no>
2026-04-10 09:49:46 +0200

    feat: install fast-syntax-highlighting, fzf, substring-search plugins

--- wsl/zsh/install-zsh.sh
+++ wsl/zsh/install-zsh.sh
@@ -13,6 +13,15 @@ git clone https://github.com/zsh-users/zsh-autosuggestions \
   "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
   "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-history-substring-search.git \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-substring-search"
+
+echo "==> Installing fzf"
+if ! command -v fzf &>/dev/null; then
+  sudo apt install -y fzf
+fi
 
 echo "==> Linking .zshrc and aliases"
 ln -sf "$REPO_ROOT/wsl/zsh/.zshrc" "$HOME/.zshrc"
